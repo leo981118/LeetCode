a003172c9febbf3767a047c9fd2e2d0a29fc7e66
Leo <stu002031@yahoo.com.tw>
2022-09-03 12:40:31 +0800

    001_two_sum(vector_map)

--- 001_two_sum(vector_map).cpp
+++ 001_two_sum(vector_map).cpp
@@ -5,15 +5,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> storedNumber;
+        //Create a map that will store vector's values as it's keys and vector's indices as its values
+        map<int, int> storedNumber;     
+        //if storedNumber[target-nums[i]] exist we can return i and storedNumber[target-nums[i]]
         for(int i  = 0; i < nums.size(); i++) {
-            if(storedNumber.find(target - nums[i]) != storedNumber.end()){
+            if(storedNumber.find(target - nums[i]) != storedNumber.end()){      
                 return {storedNumber[target - nums[i]], i};
             } 
             else{
+                //if it doesnt we add vector's value as  key, add vector's indice as value to the map 
                 storedNumber[nums[i]] = i;
             }                
         }
+        //return error
         return {-1, 1};
     }
 };
